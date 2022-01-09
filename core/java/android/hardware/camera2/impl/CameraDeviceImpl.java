b256ac88420d9813c66e12819936fafb6b522bc2
Pranav Vashi <neobuddy89@gmail.com>
2022-01-10 01:51:25 +0530

    Add config overlay to force enable multi resolution for camera
    * Apparently, required by OOS camera port.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [YAAP:] also required to fix some logical id behavior outside of oos
    camera
    
    Change-Id: I7291aae1da13da5e72b046ee5a2eb7055c569ff7

--- core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
+++ core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -190,6 +190,8 @@ public class CameraDeviceImpl extends CameraDevice
     private final int mTotalPartialCount;
     private final Context mContext;
 
+    private final boolean mForceMultiResolution;
+
     private static final long NANO_PER_SECOND = 1000000000; //ns
 
     /**
@@ -455,6 +457,9 @@ public class CameraDeviceImpl extends CameraDevice
             mTotalPartialCount = partialCount;
         }
         mIsPrivilegedApp = checkPrivilegedAppList();
+
+        mForceMultiResolution = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceMultiResolution);
     }
 
     /**
@@ -2115,7 +2120,7 @@ public class CameraDeviceImpl extends CameraDevice
             return;
         }
         int inputFormat = inputConfig.getFormat();
-        if (inputConfig.isMultiResolution()) {
+        if (inputConfig.isMultiResolution() || mForceMultiResolution) {
             MultiResolutionStreamConfigurationMap configMap = mCharacteristics.get(
                     CameraCharacteristics.SCALER_MULTI_RESOLUTION_STREAM_CONFIGURATION_MAP);
 
