b0896f44cd9424af2e8bb41672a29b330e159a84
spkal01 <kalligeross@gmail.com>
2026-05-31 17:00:13 +0300

    core: AHardwareBuffer fixes for oplus camera
    Change-Id: I8a5b5c27654690b8b14c1023126d273544ada872
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/SurfaceView.java
+++ core/java/android/view/SurfaceView.java
@@ -2778,4 +2778,8 @@ public class SurfaceView extends View implements ViewRootImpl.SurfaceChangedCall
     public CharSequence getAccessibilityClassName() {
         return SurfaceView.class.getName();
     }
+
+    /** @hide */
+    public void setExcludeSnapshot(boolean exclude) {
+    }
 }
